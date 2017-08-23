35fb73eb101f003eeda937c9343a57b346aa3c57
HUSTSWH <867217582@qq.com>
2017-08-23 22:41:02 +0800

    Dijk_with_heap
    删去了冗余的内容，精简了代码内容，只留下最精华的内容。

--- dijk_with_heap.cpp
+++ dijk_with_heap.cpp
@@ -1,10 +1,13 @@
+/***********************
+ * Dijistra with heap
+ * Time complexity: O(nlog(n) + m)
+ * Author: songwenhao
+ * Date: 2017/08/23
+ *********************/
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
 #include <vector>
-#include <map>
-#include <string>
-#include <cassert>
 #include <ext/pb_ds/priority_queue.hpp>
 
 using namespace std;
@@ -13,89 +16,43 @@ typedef heap::point_iterator myit;
 
 
 const int maxn = 100000+10;
-char stat[30+7];
-vector<pair<int, long long> > edge[maxn];
-long long dist[maxn][2];
-myit pit[maxn][2];
-int pre[maxn][2];
-bool color[maxn];
+vector<pair<int, int> > myvec[maxn];
+int dist[maxn];
+myit pit[maxn];
 
 int main ()
 {
-    int T; 
-    scanf("%d",&T);
-    heap p;
-    for(int cas=1; cas<=T; cas++){
-        int n,m;
-        scanf("%d %d",&n,&m);
-        for(int i=1; i<=n; i++){
-            edge[i].clear();
-            pit[i][0] = pit[i][1] = NULL;
-        }
-        memset(color, 0, (n+1)*sizeof(color[0]) );
-        memset(pre, 0, (n+1)*sizeof(pre[0]));
-        for(int i=0; i<m; i++){
-            int a,b,c;
-            scanf("%d %d %d",&a,&b,&c);
-            edge[a].push_back(make_pair((long long)b,c));
-        }
-        int t;
-        scanf("%d",&t);
-        for(int i=0; i<t; i++){
-            int temp;
-            scanf("%d",&temp);
-            color[temp] = true;
-        }
-        memset(dist, 0x3f, (n+1)*sizeof(dist[0]) );
-        for(int i=1; i<=n; i++) {
-            if(color[i]==0) continue;
-            pit[i][0] = p.push(make_pair(0, make_pair(i, i) ) );
-            dist[i][0] = 0;
-            pre[i][0] = i;
-        }
-        long long ans;
-        while(!p.empty()) {
-            int x = p.top().second.first;
-            int xpre = p.top().second.second;
+    int n;
+    memset(dist, 0x3f, sizeof(dist));
+    while(scanf("%d",&n)!=EOF && n!=-1){
+		// create graph
+		...
+		// create graph
+        heap p;
+        //for(int i=1; i<tag; i++)pit[i]=NULL;
+        pit[1]=p.push(pair<int, int>(0, 1));
+        dist[1]=0;
+        while(!p.empty()){
+            int x = p.top().second;
             p.pop();
-            int tag;
-            if(xpre == pre[x][0]){
-                pit[x][0] = NULL;
-                tag = 0;
-            } else {
-                assert(xpre == pre[x][1]);
-                pit[x][1] = NULL;
-                tag = 1;
-            }
-            if(color[x] && xpre != x) {
-                ans = dist[x][tag];
-                break;
-            }
-            int l = edge[x].size();
+            pit[x]=NULL;
+            int l=myvec[x].size();
             for(int i=0; i<l; i++){
-                int y = edge[x][i].first;
-                long long d = edge[x][i].second;
-                if(pre[y][1] != xpre && (pre[y][0] == 0 || dist[y][0] > dist[x][tag] + d)) {
-                    dist[y][0] = dist[x][tag] + d;
-                    pre[y][0] = xpre;
-                    if(pit[y][0] != NULL)
-                        p.modify(pit[y][0], make_pair(dist[y][0], make_pair(y, xpre) ) );
-                    else
-                        pit[y][0] = p.push(make_pair(dist[y][0], make_pair(y, xpre) ) );
-                } else if(pre[y][0] != xpre && (pre[y][1] == 0 || dist[y][1] > dist[x][tag] + d) ) {
-                    dist[y][1] = dist[x][tag] + d;
-                    pre[y][1] = xpre;
-                    if(pit[y][1] != NULL)
-                        p.modify(pit[y][1], make_pair(dist[y][1], make_pair(y, xpre) ) );
-                    else
-                        pit[y][1] = p.push(make_pair(dist[y][1], make_pair(y, xpre) ) );
-                }
+                int y=myvec[x][i].first;
+                int d=myvec[x][i].second;
+                if(dist[y]<=dist[x]+d)continue;
+                dist[y] = dist[x]+d;
+                if(pit[y]!=NULL)
+                    p.modify(pit[y], pair<int, int>(dist[y], y));
+                else
+                    pit[y] = p.push(pair<int, int>(dist[y], y));
             }
         }
-        while(!p.empty())p.pop();
-        printf("Case #%d: %lld\n", cas, ans);
+        for(int i=1; i<tag; i++)myvec[i].clear();
+        // print answer
+		...
+		// print answer
     }
     return 0;
 }
-
 
