0a1512b1dfb16b03578bffe45dd3c213d40592aa
HUSTSWH <867217582@qq.com>
2017-09-19 10:42:22 +0800

    加入NTT模板
    加入了NTT模板，用于卷积计算，没有精度损失
    部分修正FFT模板，现在抄起来更加顺手

--- FFT.cpp
+++ FFT.cpp
@@ -9,22 +9,22 @@ const int maxn = 1<<20;
 
 complex<double> a[maxn], b[maxn];
 
-void fft(complex<double> x[], const size_t len, const int &on)
+void FFT(complex<double> x[], const int len, const int &on)
 {
-    for(size_t i=1, j=len/2; i<len-1; i++) {
+    for(int i=1, j=len/2; i<len-1; i++) {
         if(i<j) swap(x[i], x[j]);
-        size_t k = len/2;
+        int k = len/2;
         while(j>=k) {
             j -= k;
             k >>=1;
         }
         if(j<k) j += k;
     }
-    for(size_t k=1; k<len; k<<=1) {
+    for(int k=1; k<len; k<<=1) {
         complex<double> wm(cos(PI/k), sin(PI/k)*on);
-        for(size_t i=0; i<len; i+=2*k) {
+        for(int i=0; i<len; i+=2*k) {
             complex<double> w(1.0,0.0);
-            for(size_t j=0; j<k; j++) {
+            for(int j=0; j<k; j++) {
                 complex<double> u = x[i+j];
                 complex<double> t = w*x[i+j+k];
                 x[i+j] = u+t;
@@ -34,7 +34,7 @@ void fft(complex<double> x[], const size_t len, const int &on)
         }
     }
     if(on == -1)
-        for(size_t i=0; i<len; i++)
+        for(int i=0; i<len; i++)
             x[i] /= (double)len;
 }
 
@@ -44,11 +44,11 @@ int main()
         a[i] = i;
         b[i] = 8-i;
     }
-    fft(a, 8, 1);
-    fft(b, 8, 1);
+    FFT(a, 8, 1);
+    FFT(b, 8, 1);
     for(int i=0; i<8; i++)
         a[i] *= b[i];
-    fft(a, 8, -1);
+    FFT(a, 8, -1);
     for(int i=0; i<8; i++)
         cout<<a[i]<<endl;
     return 0;
---
+++ NTT.cpp
@@ -0,0 +1,97 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const long long g = 3;
+const int modn = (479<<21)+1;
+const int maxn = 1<<17;
+
+#define mod(a) ((((a)%modn)+modn)%modn)
+
+int a[maxn], b[maxn];
+char s[maxn];
+
+/// note that a should be a long long if a*a may exceed int
+template<typename T>
+T quickpow(const T& a, const unsigned &n, const int P)
+{
+    if(n==1) return a;
+    T ans = quickpow(a*a%P, n>>1, P);
+    if(n&1) ans = ans * a % P;
+    return ans;
+}
+
+/// len: must be 2^k and larger than length of x
+/// note that len of NTT must be equal to len of INTT
+void NTT(int x[], const int len, const int &on)
+{
+    for(int i=1, j=len/2; i<len-1; i++) {
+        if(i<j) swap(x[i], x[j]);
+        int k = len/2;
+        while(j>=k) {
+            j -= k;
+            k >>=1;
+        }
+        if(j<k) j += k;
+    }
+    for(int k=1; k<len; k<<=1) {
+        // complex<double> wm(cos(PI/k), sin(PI/k)*on);
+        int wm = quickpow(g, (modn-1)/k/2, modn);
+        for(int i=0; i<len; i+=2*k) {
+            // complex<double> w(1.0,0.0);
+            int w = 1;
+            for(int j=0; j<k; j++) {
+                int u = x[i+j];
+                int t = mod(1LL*w*x[i+j+k]);
+                x[i+j] = mod(u+t);
+                x[i+j+k] = mod(u-t);
+                w = mod(1LL*w*wm);
+            }
+        }
+    }
+    if(on == -1) {
+        reverse(x+1, x+len);
+        int inv = quickpow(1LL*len, modn-2, modn);
+        for(int i=0; i<len; i++)
+            x[i] = mod(1LL*x[i]*inv);
+    }
+}
+
+int main()
+{
+    while(scanf("%s", s)!= EOF) {
+        int l1 = strlen(s);
+        reverse(s, s+l1);
+        for(int i=0; i<l1; i++)
+            a[i] = s[i] - '0';
+        scanf("%s", s);
+        int l2 = strlen(s);
+        reverse(s, s+l2);
+        for(int i=0; i<l2; i++)
+            b[i] = s[i] - '0';
+        
+        // get the minimum of 2^k which is greater than length of a or b
+        int n = 1<<(8*sizeof(int) - __builtin_clz(max(l1, l2) ) ); 
+        n<<=1;  //DO NOT MISS IT!!!
+        fill(a+l1, a+n, 0);
+        fill(b+l2, b+n, 0);
+        NTT(a, n, 1);
+        NTT(b, n, 1);
+        for(int i=0; i<n; i++)
+            a[i] = mod(1LL*a[i]*b[i]);
+        NTT(a, n, -1);
+        int l = 0;
+        for(int i=0; i<n; i++) {
+            a[i+1] += a[i]/10;
+            a[i] %= 10;
+            if(a[i]) l = i;
+        }
+        for(int i=0; i<=l; i++)
+            s[i] = a[i] + '0';
+        s[l+1] = 0;
+        reverse(s, s+l+1);
+        puts(s);
+    }
+    return 0;
+}
+
